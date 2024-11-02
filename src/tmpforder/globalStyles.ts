7480b18d4c7f1994ba469a9d972d308121697111
sooy <ssy67366736@gmail.com>
2024-11-02 21:00:55 +0900

    chore: tmp

---
+++ src/tmpforder/globalStyles.ts
@@ -0,0 +1,44 @@
+import { css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
+
+const globalStyles = css`
+  ${emotionReset}
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family:
+      'Pretendard Variable',
+      Pretendard,
+      -apple-system,
+      BlinkMacSystemFont,
+      system-ui,
+      Roboto,
+      'Helvetica Neue',
+      'Segoe UI',
+      'Apple SD Gothic Neo',
+      'Noto Sans KR',
+      'Malgun Gothic',
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol',
+      sans-serif;
+    font-style: normal;
+    color: #000;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+  }
+`;
+
+export default globalStyles;
