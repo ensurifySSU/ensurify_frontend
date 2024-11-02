ff24e028d76dd034b70e1ee6ed0ca9b3481cc7cc
sooy <ssy67366736@gmail.com>
2024-11-02 20:47:33 +0900

    feat: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Global, ThemeContext, ThemeProvider } from '@emotion/react';
 import globalStyles from './COMMON/styles/globalStyles';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router';
 
 function App() {
   return (
     <ThemeProvider theme={ThemeContext}>
       <Global styles={globalStyles} />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
