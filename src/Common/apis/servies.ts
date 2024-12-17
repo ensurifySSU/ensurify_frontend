34fe42a287f1a884957c2435d185487d25d6264d
Sunbeom <wsbs9805@naver.com>
2024-12-17 13:37:23 +0900

    📝 getFile, postFile API 함수 작성

--- src/Common/apis/servies.ts
+++ src/Common/apis/servies.ts
@@ -20,3 +20,21 @@ export const getClientDetailInfo = async (clientId: number | undefined) => {
   const { data } = await instance.get(`/clients/${clientId}`);
   return data;
 };
+
+export const getFileDownload = async (fileName: string | undefined) => {
+  if (!fileName) return;
+  const { data } = await instance.get(`/s3/download?fileName=${fileName}`);
+  return data;
+}
+
+export const postFileUpload = async (file: File) => {
+  const formData = new FormData();
+  formData.append('file', file);
+  const config = {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    },
+  };
+  const { data } = await instance.post(`/s3/upload`, formData, config);
+  return data;
+}
